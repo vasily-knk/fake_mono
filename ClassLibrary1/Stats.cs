72b40ec2d0fea83a4306675cf8d89c585db5049e
vasily-knk <vasily.knk@gmail.com>
2016-08-19 01:02:34 +0300

    forgotten

---
+++ ClassLibrary1/Stats.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    class Stats
+    {
+        private IList<StatsFrame> frames = new List<StatsFrame>();
+
+        public void Append(StatsFrame frame)
+        {
+            frames.Add(frame);
+        }
+    }
+}
---
+++ ClassLibrary1/StatsFrame.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace ClassLibrary1
+{
+    class StatsFrame
+    {
+//         struct ObjectData
+//         {
+//             String name;
+//             Vector3 pos;
+//         }
+// 
+//         struct PosUpdate
+//         {
+//             long id;
+//             Vector3 pos;
+//         }
+// 
+//         ObjectData[] newObjects { get; private set; }
+//         PosUpdate[] posUpdates { get; private set; }
+//         long[] deletedObjects { get; private set; }
+
+        public StatsFrame()
+        {
+        }
+    }
+}
