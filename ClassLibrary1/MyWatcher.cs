1960502a5a30946ffac741ea863a502cc109445e
vasily-knk <vasily.knk@gmail.com>
2016-08-28 19:37:44 +0300

    linq deltas

--- ClassLibrary1/MyWatcher.cs
+++ ClassLibrary1/MyWatcher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using UnityEngine;
 using System.Runtime.CompilerServices;
@@ -8,12 +9,48 @@ using System.Linq;
 
 namespace ClassLibrary1
 {
+    class ObjectDataOps
+    {
+        public static Vector3 ExtractPos(GameObject o)
+        {
+            return o.transform.position;
+        }
+    }
+    
+    struct ObjectData
+    {
+        public readonly String Name;
+        public readonly Vector3 Pos;
+
+        public ObjectData(GameObject o)
+        {
+            Name = o.name;
+            Pos = ObjectDataOps.ExtractPos(o);
+        }
+    }
+
+    struct Delta
+    {
+        public readonly KeyValuePair<long, ObjectData>[] Created;
+        public readonly long[] Deleted;
+        public readonly KeyValuePair<long, Vector3>[] DeltaPos;
+
+        public Delta(KeyValuePair<long, ObjectData>[] created, long[] deleted, KeyValuePair<long, Vector3>[] deltaPos)
+        {
+            Created = created;
+            Deleted = deleted;
+            DeltaPos = deltaPos;
+        }
+    }
+
     public class MyWatcher
         : MonoBehaviour
     {
         private int _numReports = 0;
         private long _frame = 0;
-        private WeakReference[] _oldObjectsWeak = {};
+        private Dictionary<long, ObjectData> _currentObjects = new Dictionary<long, ObjectData>();
+        private List<Delta> _deltas = new List<Delta>();
+        private readonly Stopwatch _stopwatch = new Stopwatch();
 
         public static GameObject CreateObject()
         {
@@ -39,39 +76,31 @@ namespace ClassLibrary1
 
         void FixedUpdate()
         {
-            var oldObjects = _oldObjectsWeak.Where(w => w.IsAlive).Select(w => w.Target as GameObject);
+            _stopwatch.Start();
+
             var newObjects = FindObjectsOfType<GameObject>();
+            var newObjectIDs = newObjects.Select(idForObject);
+            var removedIDs = _currentObjects.Select(r => r.Key).Except(newObjectIDs);
 
-            var removed = oldObjects.Except(newObjects);
-            var added = newObjects.Except(oldObjects);
+            var createdObjects = newObjects
+                .Where(o => !_currentObjects.ContainsKey(idForObject(o)))
+                .Select(o => new KeyValuePair<long, ObjectData>(idForObject(o), new ObjectData(o)));
 
-            if (removed.Any() || added.Any())
-            {
-                var s = new StringBuilder();
-                s.AppendFormat("\nFrame {0}\n", _frame);
+            var keptObjects = newObjects
+                .Where(o => _currentObjects.ContainsKey(idForObject(o)));
 
-                if (removed.Any())
-                {
-                    s.Append("Removed objects:\n");
+            var deltasPos = keptObjects
+                .Where(o => ObjectDataOps.ExtractPos(o) != _currentObjects[idForObject(o)].Pos)
+                .Select(o => new KeyValuePair<long, Vector3>(idForObject(o), ObjectDataOps.ExtractPos(o)));
 
-                    foreach (var o in removed)
-                        s.AppendFormat("    {0}\n", o.name);
-                }
-                
-                if (added.Any())
-                {
-                    s.Append("Added objects:\n");
+            _deltas.Add(new Delta(createdObjects.ToArray(), removedIDs.ToArray(), deltasPos.ToArray()));
 
-                    foreach (var o in added)
-                        s.AppendFormat("    {0}\n", o.name);
-                }
-
-                Class1.Print(s.ToString());
-            }
-
-            _oldObjectsWeak = newObjects.Select(o => new WeakReference(o)).ToArray();
+            _currentObjects = newObjects.ToDictionary(idForObject, o => new ObjectData(o));
 
             ++_frame;
+            _stopwatch.Stop();
+
+            updateFPS();
         }
 
         void Update()
@@ -104,6 +133,23 @@ namespace ClassLibrary1
             builder.Append(String.Format("----- End of MyWatcher report {0}\n", _numReports));
 
             Class1.Print(builder.ToString());
+        }
+
+        private long idForObject(GameObject o)
+        {
+            return o.GetInstanceID();
+        }
+
+        private void updateFPS()
+        {
+            const int numFrames = 60;
+            if (_frame % numFrames == 0)
+            {
+                var avgTicks = _stopwatch.Elapsed.Ticks / numFrames;
+                var avgMicrosecs = avgTicks / 10;
+                Class1.Print(String.Format("\nAvg FixedUpdate time: {0}\nDeltas: {1}\n", avgMicrosecs, _deltas.Count));
+                _stopwatch.Reset();
+            }
         }
 
     }
