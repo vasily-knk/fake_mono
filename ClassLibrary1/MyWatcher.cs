77b4b3442b3210d672c57c7ec8006ccfe9a49100
vasily-knk <vasily.knk@gmail.com>
2016-08-28 17:38:58 +0300

    MyWatcher FixedUpdate with linq

--- ClassLibrary1/MyWatcher.cs
+++ ClassLibrary1/MyWatcher.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
 using System.Runtime.CompilerServices;
+using System.Linq;
 
 
 namespace ClassLibrary1
@@ -9,7 +11,9 @@ namespace ClassLibrary1
     public class MyWatcher
         : MonoBehaviour
     {
-        private int numReports = 0;
+        private int _numReports = 0;
+        private long _frame = 0;
+        private WeakReference[] _oldObjectsWeak = {};
 
         public static GameObject CreateObject()
         {
@@ -32,12 +36,50 @@ namespace ClassLibrary1
             OnObjectDestroying(gameObject);
         }
 
+
+        void FixedUpdate()
+        {
+            var oldObjects = _oldObjectsWeak.Where(w => w.IsAlive).Select(w => w.Target as GameObject);
+            var newObjects = FindObjectsOfType<GameObject>();
+
+            var removed = oldObjects.Except(newObjects);
+            var added = newObjects.Except(oldObjects);
+
+            if (removed.Any() || added.Any())
+            {
+                var s = new StringBuilder();
+                s.AppendFormat("\nFrame {0}\n", _frame);
+
+                if (removed.Any())
+                {
+                    s.Append("Removed objects:\n");
+
+                    foreach (var o in removed)
+                        s.AppendFormat("    {0}\n", o.name);
+                }
+                
+                if (added.Any())
+                {
+                    s.Append("Added objects:\n");
+
+                    foreach (var o in added)
+                        s.AppendFormat("    {0}\n", o.name);
+                }
+
+                Class1.Print(s.ToString());
+            }
+
+            _oldObjectsWeak = newObjects.Select(o => new WeakReference(o)).ToArray();
+
+            ++_frame;
+        }
+
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.O))
             {
                 PrintObjects();
-                ++numReports;
+                ++_numReports;
             }
         }
 
@@ -46,7 +88,7 @@ namespace ClassLibrary1
             var objects = FindObjectsOfType<GameObject>();
 
             var builder = new StringBuilder();
-            builder.Append(String.Format("----- MyWatcher report {0}:\n", numReports));
+            builder.Append(String.Format("----- MyWatcher report {0}:\n", _numReports));
 
 //             foreach (GameObject obj in objects)
 //             {
@@ -59,7 +101,7 @@ namespace ClassLibrary1
 //                 }
 //             }
 
-            builder.Append(String.Format("----- End of MyWatcher report {0}\n", numReports));
+            builder.Append(String.Format("----- End of MyWatcher report {0}\n", _numReports));
 
             Class1.Print(builder.ToString());
         }
